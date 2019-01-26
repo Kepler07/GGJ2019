34b3bceb78bfe2ac62f05b9c667516c5a206f93a
unknown <bastienggj@D35-17-R.bellecour.int>
2019-01-26 15:24:04 +0100

    Added scripts & prefab

---
+++ MonsterController.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEditor;
+using UnityEngine;
+
+public class MonsterController : MonoBehaviour
+{
+    private float speed = 12.0f;
+    private float minDistance = 1.5f;
+
+    private bool followPlayer = false;
+    private GameObject player;
+    
+    private Color[] possibleColors =
+    {
+        Color.green, // A
+        Color.red, // B
+        Color.blue, // X
+        Color.yellow // Y
+    };
+
+    private Color color;
+
+// Use this for initialization
+    void Start()
+    {
+        color = possibleColors[UnityEngine.Random.Range(0, possibleColors.Length)];
+
+        this.GetComponent<Renderer>().material.color = color;
+    }
+
+// Update is called once per frame
+    void Update()
+    {
+        if (followPlayer && Vector3.Distance(transform.position, player.transform.position) > minDistance)
+        {
+            transform.position =
+                Vector3.MoveTowards(transform.position, player.transform.position, speed * Time.deltaTime);
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player") && !followPlayer)
+        {
+            var playerController = other.gameObject.GetComponent<PlayerController>();
+            player = playerController.objectToFollow();
+            if (playerController.addMonsterToList(this.gameObject))
+                followPlayer = true;
+        }
+    }
+
+    public string whichInputButton()
+    {
+        if (color == possibleColors[0]) // A
+            return "joystick button 0";
+        else if (color == possibleColors[1]) // B
+            return "joystick button 1";
+        else if (color == possibleColors[2]) // X
+            return "joystick button 2";
+        else // Y
+            return "joystick button 3";
+    }
+}
---
+++ PlayerController.cs
@@ -0,0 +1,112 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEditor;
+using UnityEditor.SceneManagement;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed;
+    public float brake;
+    public int MAX_MONSTERS;
+
+    private Rigidbody rb;
+
+    private float curSpeed;
+
+    private bool inputApplied;
+
+    private float moveHorizontal;
+    private float moveVertical;
+
+    private List<GameObject> monsterList = new List<GameObject>();
+    private List<string> inputList = new List<string>();
+
+    public PlayerController()
+    {
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        moveHorizontal = Input.GetAxis("Horizontal");
+        moveVertical = Input.GetAxis("Vertical");
+
+
+        if (moveHorizontal == 0.0f && moveVertical == 0.0f)
+            inputApplied = false;
+        else
+            inputApplied = true;
+
+    }
+
+    void FixedUpdate()
+    {
+        if (inputApplied)
+        {
+            Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+            rb.velocity = (movement * speed);
+            transform.rotation = Quaternion.LookRotation(-rb.velocity);
+        }
+        else
+        {
+            curSpeed = rb.velocity.magnitude;
+            float newSpeed = curSpeed - brake * Time.deltaTime;
+            if (newSpeed < 0)
+            {
+                newSpeed = 0;
+            }
+
+            rb.velocity = rb.velocity.normalized * newSpeed;
+        }
+    }
+
+    public Rigidbody Rb
+    {
+        get { return rb; }
+        set { rb = value; }
+    }
+
+    public bool addMonsterToList(GameObject monster)
+    {
+        if (!monsterList.Contains(monster) && monsterList.Count <= MAX_MONSTERS - 1)
+        {
+            monsterList.Add(monster);
+            inputList.Add(monster.GetComponent<MonsterController>().whichInputButton());
+
+            return true;
+        }
+
+        return false;
+    }
+
+    public GameObject objectToFollow()
+    {
+        if (monsterList.Count == 0)
+        {
+            return GetComponentInChildren<Transform>().gameObject;
+        }
+
+        else
+        {
+            return monsterList.Last();
+        }
+    }
+
+    public string[] getInputArray()
+    {
+        return inputList.ToArray();
+    }
+
+    public List<GameObject> MonsterList
+    {
+        get { return monsterList; }
+        set { monsterList = value; }
+    }
+}
