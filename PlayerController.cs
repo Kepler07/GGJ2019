fcc93edcca28ece5f2671ecc015f24f6273fffbb
bastienggj <psyqqow@gmail.com>
2019-01-26 23:22:18 +0100

    neg velocity

--- PlayerController.cs
+++ PlayerController.cs
@@ -52,7 +52,7 @@ public class PlayerController : MonoBehaviour
         {
             Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
             rb.velocity = (movement * speed);
-            transform.rotation = Quaternion.LookRotation(-rb.velocity);
+            transform.rotation = Quaternion.LookRotation(rb.velocity);
         }
         else
         {
