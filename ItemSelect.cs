c2054077a8f91dbf96d4f53f9060072e7dbb96cb
bastienggj <psyqqow@gmail.com>
2019-01-27 02:36:19 +0100

    menu

---
+++ ItemSelect.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ItemSelect : MonoBehaviour
+{
+
+	public Button startButton;
+	public Button quitButton;
+	public Button creditsButton;
+
+	private Color startColor;
+	private Color quitColor;
+	private Color creditsColor;
+	
+	// Use this for initialization
+	void Start ()
+	{
+		startColor = startButton.GetComponentInChildren<Text>().color;
+		quitColor = quitButton.GetComponentInChildren<Text>().color;
+		creditsColor = creditsButton.GetComponentInChildren<Text>().color;
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	
+	public void onSelectStart()
+	{
+		startButton.GetComponentInChildren<Text>().color = Color.red;
+	}
+	
+	public void onDeselectStart()
+	{
+		startButton.GetComponentInChildren<Text>().color = startColor;
+	}
+		
+	public void onSelectQuit()
+	{
+		quitButton.GetComponentInChildren<Text>().color = Color.red;
+	}
+	
+	public void onDeselectQuit()
+	{
+		quitButton.GetComponentInChildren<Text>().color = quitColor;
+	}
+		
+	public void onSelectCredits()
+	{
+		creditsButton.GetComponentInChildren<Text>().color = Color.red;
+	}
+	
+	public void onDeselectCredits()
+	{
+		creditsButton.GetComponentInChildren<Text>().color = creditsColor;
+	}
+}
---
+++ StartMenu.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class StartMenu : MonoBehaviour
+{
+
+	public Button startButton;
+	public Button quitButton;
+	public Button creditsButton;
+	public AudioSource audioSource;
+	public string playScene = "playScene";
+	public string creditsScene = "creditsScene";
+	
+	// Use this for initialization
+	private void Start () {
+		startButton.onClick.AddListener(startGame);
+		quitButton.onClick.AddListener(quitGame);
+		creditsButton.onClick.AddListener(creditsGame);
+	}
+
+
+	public void startGame()
+	{
+		Debug.Log("Start clicked");
+		audioSource.Play();
+		Invoke("loadSceneGame", 1);
+	}
+
+	public void creditsGame()
+	{
+		Debug.Log("Credits clicked");
+		audioSource.Play();
+		Invoke("loadSceneCredit", 1);
+	}
+	
+	private void loadSceneGame()
+	{
+		SceneManager.LoadScene(playScene, LoadSceneMode.Single);
+	}
+
+	private void loadSceneCredit(){
+		SceneManager.LoadScene(creditsScene, LoadSceneMode.Single);
+	}
+
+	private void quitGame()
+	{
+		Debug.Log("Quit clicked");
+		Application.Quit();
+	}
+	
+	
+}
